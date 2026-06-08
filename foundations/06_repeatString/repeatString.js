16dc5cf69a302e3b378fd26bc77cc7e3015bf1c5
pegmil <pjmilos@comcast.net>
2026-06-08 21:30:12 +0000

    Working through the javascript exercises 1-8

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function (inString, repeatNum) {
+    let returnString = "";
 
-};
+    if (repeatNum < 0) {
+        returnString = "ERROR";
+    }
+    else {
+        for (let i = 0; i < repeatNum; i++) {
+            returnString = returnString + inString;
+        }
+    }
+
+    return returnString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inArray, ...args) {
+
+    const newArray = [];
+   inArray.forEach((item) => {
+   if (!args.includes(item)) {
+      newArray.push(item);
+    }
+   })
+   return newArray;
 };
 
 // Do not edit below this line
